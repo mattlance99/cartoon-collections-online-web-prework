64ac5a2675de6d2c778791f0969eb507a5a09241
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-18 21:10:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_the_cheese(cheese_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_strings.include?("cheddar" || "gouda" || ""camembert" )
+  if cheese_strings.include?("cheddar" || "gouda" || "camembert" )
   elsif
 end
