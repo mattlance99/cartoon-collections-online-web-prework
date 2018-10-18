b46d376347c20de4dc9bb9186f7e2938fb5f28d5
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-18 20:40:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ def long_planeteer_calls(array_of_words)
   array_of_words.any? {|word| word.length >= 5}
 end 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  #cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
